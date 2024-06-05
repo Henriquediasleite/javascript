c85d3264309f847acf2d4d187b375051578963e8
Henriquediasleite <hdias8969@gmail.com>
2024-06-05 00:01:02 -0300

    .

--- aula012/script.js
+++ aula012/script.js
@@ -15,25 +15,35 @@ function verificar(){
             genero = 'Homem'
             if (idade >=0 && idade < 10) {
                 //Criança
+                img.setAttribute('src', 'menino.png')
             } else if (idade < 21) {
                 //Jovem
+                img.setAttribute('src', 'jovemh.png')
             } else if (idade < 50) {
                 //Adulto
+                img.setAttribute('src', 'homem.png')
             } else{
                 //Idoso
+                img.setAttribute('src', 'senhor.png')
             }
         } else if (fsex[1].checked) {
             genero = 'Mulher'
             if (idade >=0 && idade < 10) {
-
-            } else if ( idade < 21) {
-
+                //Criança
+                img.setAttribute('src', 'menina.png')
+            } else if (idade < 21) {
+                //Jovem
+                img.setAttribute('src', 'jovemm.png')
             } else if (idade < 50) {
-
-            } else {
-                
+                //Adulto
+                img.setAttribute('src', 'mulher.png')
+            } else{
+                //Idoso
+                img.setAttribute('src', 'senhora.png')
             }
         }
+        res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img) //Usado para adicionar um elemento
     }
 }
