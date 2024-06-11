e045e3366df118741635baae80471c4f32be97bc
Henriquediasleite <hdias8969@gmail.com>
2024-06-10 23:02:48 -0300

    Aprendendo um pouco sobre repetições

---
+++ aula13/ambiente.js
@@ -0,0 +1,21 @@
+var c = 1
+do {
+    console.log(`passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+
+/*
+var c = 1 //começa
+while (c <= 10) {
+    console.log(`Passo ${c}`) 
+    c++ // c = c + 1
+}*/
+
+
+/*console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')*/
